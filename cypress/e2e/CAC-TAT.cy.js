f89a7c603ac43f727726e7ee28f0b3bbf1f3c68e
Michele Rodrigues <michelyrodrigues451@gmail.com>
2024-07-09 11:12:09 -0300

    initial commit

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -200,7 +200,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             })
         
     })
-    it.only('Desafio encontre o gato', function (){
+    it('Desafio encontre o gato', function (){
         cy.get ('span[id=cat]') 
             .invoke('show') 
             .should('be.visible')
